7f63ddfe338462c351c54c54917ce3414560210f
junggyoo <jungggyu.1@gmail.com>
2021-01-29 18:20:11 +0900

    add: move to detail when click video

---
+++ src/components/video_detail/video_detail.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "./video_detail.module.css";
+
+function VideoDetail({ video, video: { snippet } }) {
+  return (
+    <section className={styles.detail}>
+      <iframe
+        className={styles.vedio}
+        title={snippet.title}
+        type="text/html"
+        width="100%"
+        height="500"
+        src={`https://www.youtube.com/embed/${video.id}`}
+        frameborder="0"
+        allowfullscreen
+      ></iframe>
+      <h2>{snippet.title}</h2>
+      <h3>{snippet.channelTitle}</h3>
+      <pre className={styles.description}>{snippet.description}</pre>
+    </section>
+  );
+}
+
+export default VideoDetail;
--- src/components/video_list/video_list.jsx
+++ src/components/video_list/video_list.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import VideoItem from "../video_item/video_item";
 import styles from "./video_list.module.css";
 
-function VideoList({ videos }) {
+function VideoList({ videos, onVideoClick, display }) {
   return (
     <ul className={styles.videos}>
       {videos.map((video) => (
-        <VideoItem video={video} key={video.id} />
+        <VideoItem
+          video={video}
+          key={video.id}
+          onVideoClick={onVideoClick}
+          display={display}
+        />
       ))}
     </ul>
   );
